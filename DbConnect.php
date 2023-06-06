4661891b2423f540b373eca4f30d0d6285f544b1
Aliu Adedigba <indexcorpng@gmail.com>
2023-06-06 09:25:35 +0100

    first commit

--- DbConnect.php
+++ DbConnect.php
@@ -1,31 +1,28 @@
 <?php
-    /**
-     * Database Connection
-     */
 
-  class DbConnect {
-         private $server = 'localhost';
-        
-         
-             private $dbname = 'lawebpor_api';
-             private $user = 'lawebpor_chronicl';
-             private $pass = 'LearnAfrica@2023';
-            //  private $user = 'root';
-            //  private $pass = '';
-        
-         public function connect(){
-             try{
-                 $conn = new PDO('mysql:host='. $this->server .';dbname=' .$this->dbname, $this->user, $this->pass);
-                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                 return $conn;
+/**
+ * Database Connection
+ */
 
-             } catch (\Exception $e){
-                 echo "Database Error: " . $e->getMessage();
-             }
-         }
-
-     }
+class DbConnect
+{
+    private $server = 'localhost';
 
 
+    private $dbname = 'ulearnlm_igcse';
+    private $user = 'ulearnlm_ali';
+    private $pass = 'FuTa@(2017)';
+    //  private $user = 'root';
+    //  private $pass = '';
 
-?>
+    public function connect()
+    {
+        try {
+            $conn = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->dbname, $this->user, $this->pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (\Exception $e) {
+            echo "Database Error: " . $e->getMessage();
+        }
+    }
+}
