7bb9c4c622f6b394115ce8f9877aef4c3ac5eb81
Aliu Adedigba <indexcorpng@gmail.com>
2023-06-14 12:35:16 +0100

    dashboard

--- Query.php
+++ Query.php
@@ -32,11 +32,11 @@ class  Query
     public function get_question_s($tt_id)
     {
 
-        // $stmt = $this->pdo->prepare("SELECT marking.id, questions.questionText, questions.optionA, questions.optionB, questions.optionC, questions.OptionD, questions.images FROM marking  inner join questions  ON  marking.question_id = questions.id where  marking.test_taken_id = $tt_id  ORDER BY marking.id ASC");
+        $stmt = $this->pdo->prepare("SELECT marking.id, questions.questionText, questions.optionA, questions.optionB, questions.optionC, questions.OptionD, questions.images FROM marking  inner join questions  ON  marking.question_id = questions.id where  marking.test_taken_id = $tt_id  ORDER BY marking.id ASC");
 
         //$stmt = $this->pdo->prepare("SELECT marking.id, questions.questionText, questions.optionA, questions.optionB, questions.optionC, questions.OptionD, questions.images FROM marking   INNER JOIN questions  ON  marking.question_id = questions.id where  marking.test_taken_id = $tt_id ");
 
-        $stmt = $this->pdo->prepare("SELECT * FROM marking   where  test_taken_id = $tt_id ");
+        // $stmt = $this->pdo->prepare("SELECT * FROM marking   where  test_taken_id = $tt_id ");
 
 
         $stmt->execute();
